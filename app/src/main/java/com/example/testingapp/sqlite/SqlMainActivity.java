404d9872a06f609b4466d121958629885382d1d9
mjh2550 <moomjh94@naver,com>
2022-04-05 07:13:53 +0900

    sql 프로세스

--- app/src/main/java/com/example/testingapp/sqlite/SqlMainActivity.java
+++ app/src/main/java/com/example/testingapp/sqlite/SqlMainActivity.java
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.testingapp.R;
@@ -15,7 +17,8 @@ public class SqlMainActivity extends AppCompatActivity {
 
     SQLitePersonDao dao = null;
     ArrayList<PersonVo> p_list = null;
-    RecyclerView rv_list = null;
+    //RecyclerView rv_list = null;
+    ListView lv_list = null;
 
 
     @Override
@@ -23,11 +26,32 @@ public class SqlMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sql_main);
 
-        rv_list = findViewById(R.id.recycler_view);
-        rv_list.setLayoutManager(new LinearLayoutManager(this));
-       // rv_list.setAdapter(new Recy);
+        Button button = findViewById(R.id.button2);
+        lv_list = findViewById(R.id.lv_list);
+        dao = new SQLitePersonDao(this);
 
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
+
+        //리스트 데이터 불러오기
+        display_person_list();
+
+        // rv_list = findViewById(R.id.recycler_view);
+        // rv_list.setLayoutManager(new LinearLayoutManager(this));
+        // rv_list.setAdapter(new Recy);
+
+    }
+
+    /**
+     * 리스트 데이터 바인드 메서드
+     */
+    public void display_person_list(){
+        p_list = dao.selectList();
+        lv_list.setAdapter(new PersonAdapter());
     }
 }
