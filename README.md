3d02a0177a169a5d8f41d82a0e9df877d3f32550
jhmoon <moomjh94@naver.com>
2022-05-31 13:13:47 +0900

    1

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
-# app/kotlin/ingang3
-인프런 안드로이드 인강 클론코딩
+# app/kotlin/JCEx
+Android Jetpack Compose Example
 
-# AACCodeLab
-Android Architecture Component CodeLab
+### Reference URL : 
 
-https://developer.android.com/codelabs/android-room-with-a-view-kotlin#0
+-@Composable 콤포즈 UI뷰를 반환
+
+-@Preview 미리보기
+
+-콤포저블 단위/ flutter, swiftUI와 매우 유사
+
+-특이점 : 플러터와 마찬가지로 scafold 지원
+Scafold(
+    //design source
+){
+    Text(text = "텍스트")
+}
+
+-스택 Column, Row, 겹치기 Box 가 뷰의 핵심요소이다.
--- app/src/main/java/com/mjh/jcexample/MainActivity.kt
+++ app/src/main/java/com/mjh/jcexample/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.mjh.jcexample
 
+import android.inputmethodservice.Keyboard
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mjh.jcexample.ui.theme.JCExampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,8 +37,52 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Scaffold(topBar = {
+        TopAppBar(title = { Text(text = "Hello")},
+            backgroundColor = Color(0xfff25287))}
+    ) {
+//        Text(text = "Hello $name!")
+        MyComposableView()
+    }
 }
+@Composable
+fun MyRowItem(){
+    //호리젠탈 리니어
+    Row(
+        Modifier
+            .padding(10.dp)
+            .background(Color(0x0ffeaffd0)),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Text(text = "안녕하세요?",
+            Modifier
+                .padding(all = 10.dp)
+                .background(Color.Yellow))
+        Spacer(modifier = Modifier.size(10.dp))
+        Text(text = "안녕하세요?")
+        Text(text = "안녕하세요?")
+    }
+}
+
+
+@Composable
+fun MyComposableView(){
+    Log.d("TAG","MyComposableView")
+
+    //버티컬리니어
+    Column(
+        Modifier
+            .background(Color.Green)
+            .verticalScroll(rememberScrollState()) //스크롤
+            .fillMaxWidth() //너비 풀 사이즈
+    ) {
+        for (i in 0..20) {
+            MyRowItem()
+        }
+    }
+}
+
+
 
 @Preview(showBackground = true, name = "Text Preview")
 @Composable
