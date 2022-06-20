58d363dda85f66de872443feb06fbbc83489b8ae
jhmoon <moomjh94@naver.com>
2022-06-20 18:08:24 +0900

    FCM

--- app/src/main/java/com/mjh/rxexam/fcm/FcmMainActivity.kt
+++ app/src/main/java/com/mjh/rxexam/fcm/FcmMainActivity.kt
@@ -2,10 +2,23 @@ package com.mjh.rxexam.fcm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.mjh.rxexam.R
 
 class FcmMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fcm_main)
+
+        val tv_01 = findViewById<TextView>(R.id.tv_01)
+        val btn_01 = findViewById<Button>(R.id.btn_01)
+        btn_01.setOnClickListener{
+            //TODO FCM 푸시보내기
+        }
+
+
+
     }
 }
--- app/src/main/java/com/mjh/rxexam/fcm/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/mjh/rxexam/fcm/MyFirebaseMessagingService.kt
@@ -1,4 +1,18 @@
 package com.mjh.rxexam.fcm
 
-class MyFirebaseMessagingService {
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService() : FirebaseMessagingService() {
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        if(message.data.isNotEmpty())Log.d("fcm","$message")
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.d("fcm","$token")
+    }
 }
