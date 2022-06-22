66f3ea805fc33c43fdf56f8bead54f54ae5b710b
jhmoon <moomjh94@naver.com>
2022-06-22 18:02:55 +0900

    lazycolumn 구현

--- app/src/main/java/com/mjh/rxexam/compose/JcListViewActivity.kt
+++ app/src/main/java/com/mjh/rxexam/compose/JcListViewActivity.kt
@@ -3,14 +3,20 @@ package com.mjh.rxexam.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mjh.rxexam.compose.ui.theme.RxExamTheme
 
 class JcListViewActivity : ComponentActivity() {
@@ -25,25 +31,48 @@ class JcListViewActivity : ComponentActivity() {
 @Composable
 fun jcListView(){
     RxExamTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colors.background
-        ) {
-            Greeting2("Android")
+        //LazyColumn은 화면에 보여지는 컴포저블만을 표시하는 scrollable한 column
+        LazyColumn(){
+            //한개씩 표현할 땐 item, 여러개 반복은 items , custom은 itemsIndex
+            /*item {
+                jcCard(order = 0)
+                jcCard(order = 1)
+            }*/
+            /*items(2000){
+                jcCard(order = it) //index 는 it
+            }*/
+            itemsIndexed(
+                listOf(100,200,300) //아이템 여러개생성
+            ){ index, item ->
+                jcCard(order = item) //아이템 기반으로 컴포저블 생성
+                jcCard(order = index) //아이템 기반으로 컴포저블 생성
+            }
+        }
+    }
+}
+
+@Composable
+private fun jcCard(order :Int) {
+    Card(
+        Modifier
+            .padding(12.dp)
+            .border(width = 4.dp, color = Color.Black)
+            .fillMaxWidth()
+            .height(100.dp)
+    ) {
+        Box(contentAlignment = Alignment.Center) {
+            Greeting2("Android $order")
         }
     }
 }
 
 @Composable
 fun Greeting2(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Helloooooo $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview2() {
-    RxExamTheme {
-        Greeting2("Android")
-    }
+    jcListView()
 }
